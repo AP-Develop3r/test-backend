afe98ff54b5a857c018e664617ffeb91867597a9
Anyelo Dev <AnyeloRojas93@gmail.com>
2023-08-16 12:28:12 -0500

    first commit

---
+++ database/migrations/2023_08_16_172124_create_episodios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEpisodiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('serie_id');
+            $table->string('titulo');
+            $table->integer('numero_episodio');
+            $table->text('descripcion');
+            $table->timestamps();
+            $table->foreign('serie_id')->references('id')->on('series')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodios');
+    }
+}
